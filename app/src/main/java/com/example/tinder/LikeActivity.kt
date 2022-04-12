194db809b895da3ec5a739a1117a582190a27dc6
JUNG <maestro214@naver.com>
2022-04-13 01:23:44 +0900

    Written in Arctic Fox Version

---
+++ app/src/main/java/com/example/tinder/LikeActivity.kt
@@ -0,0 +1,237 @@
+package com.example.tinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.tinder.DBKey.Companion.DIS_LIKE
+import com.example.tinder.DBKey.Companion.LIKED_BY
+import com.example.tinder.DBKey.Companion.NAME
+import com.example.tinder.DBKey.Companion.USERS
+import com.example.tinder.DBKey.Companion.USER_ID
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
+
+class LikeActivity : AppCompatActivity(), CardStackListener {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var userDB: DatabaseReference
+
+    private val adapter = CardItemAdapter()
+    private val cardItems = mutableListOf<CardItem>()
+
+    private val manager by lazy {
+        CardStackLayoutManager(this, this)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_like)
+
+        userDB = Firebase.database.reference.child(USERS)
+
+        val currentUserDB = userDB.child(getCurrentUserID())
+        currentUserDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child(NAME).value == null) {
+                    showNameInputPopup()
+                    return
+                } else {
+                }
+                getUnSelectedUser()
+                // TOdo 유저정보를 갱신해라
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+        initCardStackView()
+        initSignOutButton()
+        initMatchedListButton()
+    }
+
+    private fun initCardStackView() {
+        val stackView = findViewById<CardStackView>(R.id.cardStackView)
+
+        stackView.layoutManager = manager
+        stackView.adapter = adapter
+    }
+
+    private fun initSignOutButton(){
+        val signOutButton = findViewById<Button>(R.id.signOutButton)
+        signOutButton.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+    private fun initMatchedListButton(){
+        val matchedListButton = findViewById<Button>(R.id.matchListButton)
+        matchedListButton.setOnClickListener {
+            startActivity(Intent(this,MatchedUserActivity::class.java))
+        }
+    }
+
+    private fun getUnSelectedUser() {
+        userDB.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                if (snapshot.child(USER_ID).value != getCurrentUserID()
+                    && snapshot.child(LIKED_BY).child(LIKED_BY).hasChild(getCurrentUserID()).not()
+                    && snapshot.child(LIKED_BY).child(DIS_LIKE).hasChild(getCurrentUserID()).not()
+                ) {
+                    val userId = snapshot.child(USER_ID).value.toString()
+                    var name = "undecided"
+                    if (snapshot.child(NAME).value != null) {
+                        name = snapshot.child(NAME).value.toString()
+                    }
+                    cardItems.add(CardItem(userId, name))
+                    adapter.submitList(cardItems)
+                    adapter.notifyDataSetChanged()
+                }
+
+
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                cardItems.find { it.userId == snapshot.key }?.let {
+                    it.name = snapshot.child(NAME).value.toString()
+                }
+
+                adapter.submitList(cardItems)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    private fun showNameInputPopup() {
+        val editText = EditText(this)
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.write_name))
+            .setView(editText)
+            .setPositiveButton("저장") { _, _ ->
+                if (editText.text.isEmpty()) {
+                    showNameInputPopup()
+                } else {
+                    saveUserName(editText.text.toString())
+                }
+            }
+            .setCancelable(false)
+            .show()
+
+    }
+
+    private fun saveUserName(name: String) {
+
+        val userId = getCurrentUserID()
+        val currentUserDB = userDB.child(userId)
+        val user = mutableMapOf<String, Any>()
+        user["userId"] = userId
+        user["name"] = name
+        currentUserDB.updateChildren(user)
+
+        getUnSelectedUser()
+        // TOdo 유저정보를 가져와라
+    }
+
+
+    private fun getCurrentUserID(): String {
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "로그인이 되어있지 않습니다.", Toast.LENGTH_LONG).show()
+            finish()
+        }
+        return auth.currentUser?.uid.orEmpty()
+    }
+
+    private fun like() {
+        val card = cardItems[manager.topPosition - 1]
+        cardItems.removeFirst()
+
+        userDB.child(card.userId)
+            .child("likedBy")
+            .child("like")
+            .child(getCurrentUserID())
+            .setValue(true)
+
+        saveMatchIfOtherUserLikeMe(card.userId)
+        //todo 매칭이 된 시점을 봐야한다.
+
+        Toast.makeText(this, "${card.name}님을 Like 하셨습니다", Toast.LENGTH_LONG).show()
+    }
+
+    private fun dislike() {
+        val card = cardItems[manager.topPosition - 1]
+        cardItems.removeFirst()
+
+        userDB.child(card.userId)
+            .child("likedBy")
+            .child("dislike")
+            .child(getCurrentUserID())
+            .setValue(true)
+
+
+        Toast.makeText(this, "${card.name}님을 disLike 하셨습니다", Toast.LENGTH_LONG).show()
+    }
+
+    private fun saveMatchIfOtherUserLikeMe(otherUserId: String) {
+        val otherUserDB = userDB.child(getCurrentUserID()).child("likedBy").child("like").child(otherUserId)
+        otherUserDB.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+               if(snapshot.value == true){
+                   userDB.child(getCurrentUserID())
+                       .child("likedBy")
+                       .child("match")
+                       .child(otherUserId)
+                       .setValue(true)
+
+                   userDB.child(getCurrentUserID())
+                       .child("likeBy")
+                       .child("match")
+                       .child(getCurrentUserID())
+                       .setValue(true)
+               }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {}
+
+    override fun onCardSwiped(direction: Direction?) {
+        when (direction) {
+            Direction.Right -> like()
+            Direction.Left -> dislike()
+            else -> {
+
+            }
+        }
+
+    }
+
+    override fun onCardRewound() {}
+
+    override fun onCardCanceled() {}
+
+    override fun onCardAppeared(view: View?, position: Int) {}
+
+    override fun onCardDisappeared(view: View?, position: Int) {}
+}
